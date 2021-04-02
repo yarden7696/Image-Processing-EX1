d1c7179735f7e30e7af0c2c32d7a4704d2d2d306
Yarden Cohen <57362284+yarden7696@users.noreply.github.com>
2021-04-02 16:57:51 +0300

    Add files via upload

--- ex1_utils.py
+++ ex1_utils.py
@@ -133,14 +133,14 @@ def hsitogramEqualize(imgOrig: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarra
         :return: (imgEq,histOrg,histEQ)
     """
 
-    #show_img(imgOrig) # display the input image
+    #display_img(imgOrig) # display the input image
 
     isRGB = bool(imgOrig.ndim == 3)  # case RGB image
     if isRGB:
         imgYIQ = transformRGB2YIQ(imgOrig)
         imgOrig = np.copy(imgYIQ[:, :, 0])  # Y channel of the YIQ image
     else:
-        imgYIQ=imgOrig
+        imgYIQ=imgOrig # case grayscale
 
     imgOrig = imgOrig * 255
     imgOrig = (np.around(imgOrig)).astype('uint8')
@@ -166,7 +166,10 @@ def hsitogramEqualize(imgOrig: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarra
     return imgEq, histOrg, histEQ
 
 
-def quantizeImage(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarray], List[float]):
+
+
+
+
     """
         Quantized an image in to **nQuant** colors
         :param imOrig: The original image (RGB or Gray scale)
@@ -174,4 +177,70 @@ def quantizeImage(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarr
         :param nIter: Number of optimization loops
         :return: (List[qImage_i],List[error_i])
     """
+def quantizeImage(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarray], List[float]):
+
+
+    isRGB = bool(imOrig.ndim == 3)  # case RGB image
+    if isRGB:
+        imgYIQ = transformRGB2YIQ(imOrig)
+        imOrig = np.copy(imgYIQ[:, :, 0])  # Y channel of the YIQ image
+    else:
+        imgYIQ = imOrig  # case grayscale
+
+
+    if np.amax(imOrig) <= 1 :
+        imOrig = imOrig * 255  # normalized
+    imOrig = imOrig.astype('uint8')
+
+    histORGN, bins = np.histogram(imOrig, 256, [0, 255])
+
+    # find the boundaries
+    size = int(255 / nQuant)  # Divide the intervals evenly
+    _Z = np.zeros(nQuant + 1, dtype=int)  # z is an array that will represents the boundaries
+    for i in range(1, nQuant):
+        _Z[i] = _Z[i - 1] + size
+    _Z[nQuant] = 255  # The left border will always start at 0 and the right border will always end at 255
+
+    _Q = np.zeros(nQuant)  # _Q is an array that represent the values of the boundaries
+
+    quantized_lst = list()
+    MSE_lst = list()
+
+    for i in range(nIter):
+        _newImg = np.zeros(imOrig.shape)
+
+        for j in range(len(_Q)):  # j is a cell
+            if j == len(_Q) - 1:
+                right_cell = _Z[j + 1] + 1
+            else:
+                right_cell = _Z[j + 1]
+            range_cell = np.arange(_Z[j], right_cell)
+            _Q[j] = np.average(range_cell, weights=histORGN[_Z[j]:right_cell])
+            check = np.logical_and(imOrig >= _Z[j], imOrig < right_cell)
+            _newImg[check] = _Q[j]
+
+        imOr=imOrig / 255.0
+        imNew= _newImg / 255.0
+        pixels = imOr.size
+        subtract = np.subtract(imNew, imOr)
+        sum_pixels = np.sum(np.square(subtract))
+        MSE = np.sqrt(sum_pixels) / pixels
+        MSE_lst.append(MSE)
+
+        if isRGB:
+            _newImg=_newImg / 255.0
+            imgYIQ[:, :, 0] = _newImg
+            _newImg = transformYIQ2RGB(imgYIQ)
+        quantized_lst.append(_newImg)
+
+        for b in range(1, len(_Z) - 1):  # b is boundary
+            _Z[b] = (_Q[b - 1] + _Q[b]) / 2
+        if len(MSE_lst) >= 2:
+            if np.abs(MSE_lst[-1] - MSE_lst[-2]) <= 0.000001:
+                break
+
+    plt.plot(MSE_lst)
+    plt.show()
+    return quantized_lst, MSE_lst
     pass
+
